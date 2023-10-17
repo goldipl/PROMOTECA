979a41dc9cdf3dc1e2a1c970910830568b9ea3ec
goldipl <mgodlewski92@gmail.com>
2023-10-17 13:58:10 +0200

    clean topbar img structure

--- components/topbar.php
+++ components/topbar.php
@@ -2,7 +2,7 @@
     <div class="topbar_container">
         <div class="topbar_container_slot">
             <div class="topbar_container_slot--img">
-                <img src="./assets/icons/topbar-telephone.svg" alt="telephone" width="15" height="15">
+                <img src="./assets/icons/topbar/topbar-telephone.svg" alt="telephone" width="15" height="15">
             </div>
             <div class="topbar_container_slot--text">
                 <a href="tel:+22 486 34 83">22 486 34 83</a>
@@ -11,7 +11,7 @@
         <div class="topbar_container_line"></div>
         <div class="topbar_container_slot">
             <div class="topbar_container_slot--img">
-                <img src="./assets/icons/topbar-clock.svg" alt="clock" width="15" height="15">
+                <img src="./assets/icons/topbar/topbar-clock.svg" alt="clock" width="15" height="15">
             </div>
             <div class="topbar_container_slot--text">
                 <p>9:00 - 17:00</p>
@@ -20,7 +20,7 @@
         <div class="topbar_container_line"></div>
         <div class="topbar_container_slot">
             <div class="topbar_container_slot--img">
-                <img src="./assets/icons/topbar-envelope.svg" alt="envelope" width="15" height="15">
+                <img src="./assets/icons/topbar/topbar-envelope.svg" alt="envelope" width="15" height="15">
             </div>
             <div class="topbar_container_slot--text">
                 <a href="mailto:Info@promoteca.pl">Info@promoteca.pl</a>
@@ -30,7 +30,7 @@
         <div class="topbar_container_slot">
             <div class="topbar_container_slot--img" width="15" height="15">
                 <a href="https://www.facebook.com/PromotecaPL/">
-                    <img src="./assets/icons/topbar-facebook.svg" alt="fb">
+                    <img src="./assets/icons/topbar/topbar-facebook.svg" alt="fb">
                 </a>
             </div>
         </div>
