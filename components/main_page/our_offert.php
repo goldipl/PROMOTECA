20d71aba0819eb8e5be304cd37007ba8f629a3bd
goldipl <mgodlewski92@gmail.com>
2023-10-17 12:29:01 +0200

    styling & add our offert section

--- components/main_page/our_offert.php
+++ components/main_page/our_offert.php
@@ -1,3 +1,226 @@
 <section id="our_offert">
     <h1 class="mainpage-title">Nasza oferta</h1>
+    <div class="vertical_listings">
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image01.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Gadżety biurowe</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Akcesoria biurowe</a>
+                <a href="" class="link">Antystresy</a>
+                <a href="" class="link">Karteczki samoprzylepne</a>
+                <a href="" class="link">Lupy</a>
+                <a href="" class="link">Opakowania podarunkowe</a>
+                <a href="" class="link">Smycze reklamowe</a>
+                <a href="" class="link">Teczki konferencyjne</a>
+                <a href="" class="link">Wizytowniki</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image02.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Gadżety domowe</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Akcesoria domowe</a>
+                <a href="" class="link">Akcesoria kuchenne</a>
+                <a href="" class="link">Akcesoria do wina</a>
+                <a href="" class="link">Apteczki</a>
+                <a href="" class="link">Bombki reklamowe</a>
+                <a href="" class="link">Słodycze reklamowe</a>
+                <a href="" class="link">Zapalniczki reklamowe</a>
+                <a href="" class="link">Koce Domowe</a>
+                <a href="" class="link">Parasole automatyczne</a>
+                <a href="" class="link">Parasole manualne</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image03.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Gadżety elektroniczne</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Adaptery podróżne</a>
+                <a href="" class="link">Akcesoria elektroniczne</a>
+                <a href="" class="link">Akcesoria komputerowe</a>
+                <a href="" class="link">Akcesoria do telefonów</a>
+                <a href="" class="link">Głośniki</a>
+                <a href="" class="link">Kalkulatory</a>
+                <a href="" class="link">Kamery</a>
+                <a href="" class="link">Lokalizatory kluczy</a>
+                <a href="" class="link">Monitory aktywności</a>
+                <a href="" class="link">Powerbanki reklamowe</a>
+                <a href="" class="link">Słuchawki</a>
+                <a href="" class="link">Stacje pogodowe</a>
+                <a href="" class="link">Wskaźniki laserowe</a>
+                <a href="" class="link">Zegary reklamowe</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image04.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Gadżety podróżne</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Akcesoria podróżne</a>
+                <a href="" class="link">Chusteczki i żele do rąk</a>
+                <a href="" class="link">Etui na karty kredytowe</a>
+                <a href="" class="link">Kosmetyczki</a>
+                <a href="" class="link">Lusterka</a>
+                <a href="" class="link">Portfele</a>
+                <a href="" class="link">Szczoteczki do zębów</a>
+                <a href="" class="link">Szczotki do włosów</a>
+                <a href="" class="link">Wagi bagażowe</a>
+                <a href="" class="link">Zagłówki</a>
+                <a href="" class="link">Zawieszki do bagażu</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image05.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Gadżety sportowe i wypoczynkowe</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Akcesoria dla psa</a>
+                <a href="" class="link">Zabawki</a>
+                <a href="" class="link">Gadżety sportowe</a>
+                <a href="" class="link">Gadżety plażowe</a>
+                <a href="" class="link">Grill i piknik</a>
+                <a href="" class="link">Kompasy</a>
+                <a href="" class="link">Krzesła turystyczne</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image06.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Breloki reklamowe</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Breloki antystresy</a>
+                <a href="" class="link">Breloki kształty</a>
+                <a href="" class="link">Breloki latarki i lampki</a>
+                <a href="" class="link">Breloki miarki</a>
+                <a href="" class="link">Breloki odblaskowe</a>
+                <a href="" class="link">Breloki otwieracze do butelek</a>
+                <a href="" class="link">Breloki wielofunkcyjne</a>
+                <a href="" class="link">Breloki z żetonem do wózka</a>
+                <a href="" class="link">Inne breloki</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image07.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Narzędzia reklamowe</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Akcesoria rowerowe</a>
+                <a href="" class="link">Akcesoria samochodowe</a>
+                <a href="" class="link">Inne narzędzia</a>
+                <a href="" class="link">Lampki i latarki</a>
+                <a href="" class="link">Miarki reklamowe</a>
+                <a href="" class="link">Narzędzia wielofunkcyjne</a>
+                <a href="" class="link">Noże i scyzoryki</a>
+                <a href="" class="link">Śrubokręty</a>
+                <a href="" class="link">Zestawy narzędzi</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image08.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Torby i plecaki</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Nerki reklamowe</a>
+                <a href="" class="link">Plecaki reklamowe</a>
+                <a href="" class="link">Torby na laptopa i dokumenty</a>
+                <a href="" class="link">Torby na ramię</a>
+                <a href="" class="link">Torby reklamowe</a>
+                <a href="" class="link">Torby podróżne i sportowe</a>
+                <a href="" class="link">Torby termoizolacyjne</a>
+                <a href="" class="link">Walizki i torby na kółkach</a>
+                <a href="" class="link">Worki ze sznurkiem i żeglarskie</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image09.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Artykuły piśmiennicze</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Etui na długopisy</a>
+                <a href="" class="link">Kredki</a>
+                <a href="" class="link">Ołówki reklamowe</a>
+                <a href="" class="link">Pióra kulkowe, rollery</a>
+                <a href="" class="link">Zakreślacze i flamastry</a>
+                <a href="" class="link">Zestawy piśmiennicze</a>
+                <a href="" class="link">Długopisy reklamowe</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image10.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Odzież reklamowa</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Czapki reklamowe</a>
+                <a href="" class="link">Kamizelki</a>
+                <a href="" class="link">Koszulki</a>
+                <a href="" class="link">Kurtki</a>
+                <a href="" class="link">Peleryny, poncza</a>
+                <a href="" class="link">Rękawiczki i szaliki</a>
+                <a href="" class="link">Maseczki ochronne</a>
+                <a href="" class="link">Płyny antybakteryjne</a>
+                <a href="" class="link">Żele antybakteryjne</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image11.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Kubki i bidony</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Bidony</a>
+                <a href="" class="link">Kubki reklamowe</a>
+                <a href="" class="link">Piersiówki</a>
+                <a href="" class="link">Termosy</a>
+                <a href="" class="link">Zestawy</a>
+            </div>
+        </div>
+        <div class="vertical_listings-slot">
+            <div class="slot--image">
+                <img src="./assets/img/our_offert/image12.jpg" alt="" width="230" height="125">
+            </div>
+            <div class="slot--title">
+                <p>Gadżety dla dzieci</p>
+            </div>
+            <div class="slot--links">
+                <a href="" class="link">Artykuły szkolne</a>
+                <a href="" class="link">Gry i zabawki</a>
+                <a href="" class="link">Maskotki reklamowe</a>
+            </div>
+        </div>
+    </div>
 </section>
