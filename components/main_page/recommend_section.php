3742dd1274fb5343d1d9761e3478535e5b68dd3f
goldipl <mgodlewski92@gmail.com>
2023-10-17 10:50:22 +0200

    styling recommend section & fill template

--- components/main_page/recommend_section.php
+++ components/main_page/recommend_section.php
@@ -1,24 +1,117 @@
 <section id="recommend">
     <h1 class="mainpage-title">Polecamy</h1>
     <div class="products_container">
-        <div class="products_container--slot">
+        <div class="products_container--slot new">
             <div class="products_wrapper">
                 <div class="products_wrapper--img">
-                    <img src="" alt="">
+                    <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
                 </div>
                 <div class="products_wrapper--title">
                     <p>Antystres "kostka" V2704-08</p>
                 </div>
                 <div class="products_wrapper--colors">
                     <div class="color_square white"></div>
-                    <div class="color_square black"></div>
+                    <div class="color_square darkblue"></div>
                     <div class="color_square red"></div>
                     <div class="color_square yellow"></div>
                 </div>
                 <div class="products_wrapper--btn">
-                    <div class="more-btn">
-                        <a href="#">Więcej</a>
-                    </div>
+                    <a href="#">Więcej</a>
+                </div>
+            </div>
+        </div>
+        <div class="products_container--slot">
+            <div class="products_wrapper">
+                <div class="products_wrapper--img">
+                    <img src="./assets/img/recommend/notepad.jpg" alt="Bambusowy notatnik ok. A6 V2967-16">
+                </div>
+                <div class="products_wrapper--title">
+                    <p>Bambusowy notatnik ok. A6 V2967-16</p>
+                </div>
+                <div class="products_wrapper--colors">
+                    <div class="color_square white"></div>
+                    <div class="color_square darkblue"></div>
+                    <div class="color_square red"></div>
+                    <div class="color_square yellow"></div>
+                </div>
+                <div class="products_wrapper--btn">
+                    <a href="#">Więcej</a>
+                </div>
+            </div>
+        </div>
+        <div class="products_container--slot">
+            <div class="products_wrapper">
+                <div class="products_wrapper--img">
+                    <img src="./assets/img/recommend/document_clip.jpg" alt="Klips do dokumentów V2794-11">
+                </div>
+                <div class="products_wrapper--title">
+                    <p>Klips do dokumentów V2794-11</p>
+                </div>
+                <div class="products_wrapper--colors">
+                    <div class="color_square white"></div>
+                    <div class="color_square darkblue"></div>
+                    <div class="color_square red"></div>
+                    <div class="color_square yellow"></div>
+                </div>
+                <div class="products_wrapper--btn">
+                    <a href="#">Więcej</a>
+                </div>
+            </div>
+        </div>
+        <div class="products_container--slot">
+            <div class="products_wrapper">
+                <div class="products_wrapper--img">
+                    <img src="./assets/img/recommend/green_pen.jpg" alt="Długopis El Primero Solid EP-SOLID-09">
+                </div>
+                <div class="products_wrapper--title">
+                    <p>Długopis El Primero Solid EP-SOLID-09</p>
+                </div>
+                <div class="products_wrapper--colors">
+                    <div class="color_square white"></div>
+                    <div class="color_square darkblue"></div>
+                    <div class="color_square red"></div>
+                    <div class="color_square yellow"></div>
+                </div>
+                <div class="products_wrapper--btn">
+                    <a href="#">Więcej</a>
+                </div>
+            </div>
+        </div>
+        <div class="products_container--slot">
+            <div class="products_wrapper">
+                <div class="products_wrapper--img">
+                    <img src="./assets/img/recommend/silver_green_pen.jpg" alt="Długopis AP1001-09">
+                </div>
+                <div class="products_wrapper--title">
+                    <p>Długopis AP1001-09</p>
+                </div>
+                <div class="products_wrapper--colors">
+                    <div class="color_square white"></div>
+                    <div class="color_square darkblue"></div>
+                    <div class="color_square red"></div>
+                    <div class="color_square yellow"></div>
+                </div>
+                <div class="products_wrapper--btn">
+                    <a href="#">Więcej</a>
+                </div>
+            </div>
+        </div>
+        <div class="products_container--slot">
+            <div class="products_wrapper">
+                <div class="products_wrapper--img">
+                    <img src="./assets/img/recommend/white_green_pen.jpg" alt="Długopis AP4515W-09">
+                </div>
+                <div class="products_wrapper--title">
+                    <p>Długopis AP4515W-09</p>
+                </div>
+                <div class="products_wrapper--colors">
+                    <div class="color_square white"></div>
+                    <div class="color_square darkblue"></div>
+                    <div class="color_square red"></div>
+                    <div class="color_square yellow"></div>
+                </div>
+                <div class="products_wrapper--btn">
+                    <a href="#">Więcej</a>
                 </div>
             </div>
         </div>
