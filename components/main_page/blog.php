8c97fc2ce387cbe9cf924d88c35b3973eeebce2e
goldipl <mgodlewski92@gmail.com>
2023-10-17 12:53:27 +0200

    add blog section & styles

--- components/main_page/blog.php
+++ components/main_page/blog.php
@@ -1,6 +1,47 @@
 <section id="blog">
     <h1 class="mainpage-title">Blog</h1>
     <div class="blog_container">
-
+        <div class="article_slot">
+            <div class="article_slot--img">
+                <img src="./assets/img/blog/blog01.jpg" alt="blog img" width="503" height="346">
+            </div>
+            <div class="article_slot--title">
+                <p>Gadżety dla studentów na nadchodzący rok akademicki</p>
+            </div>
+            <div class="article_slot--text">
+                <p>04-10-2021 | Artykuły | <a href="">Promoteca</a></p>
+            </div>
+            <div class="article_slot--button">
+                <a href="#">Więcej</a>
+            </div>
+        </div>
+        <div class="article_slot">
+            <div class="article_slot--img">
+                <img src="./assets/img/blog/blog02.jpg" alt="blog img" width="503" height="346">
+            </div>
+            <div class="article_slot--title">
+                <p>Gadżety reklamowe dla miłośników dobrej kuchni!</p>
+            </div>
+            <div class="article_slot--text">
+                <p>04-10-2021 | Artykuły | <a href="">Promoteca</a></p>
+            </div>
+            <div class="article_slot--button">
+                <a href="#">Więcej</a>
+            </div>
+        </div>
+        <div class="article_slot">
+            <div class="article_slot--img">
+                <img src="./assets/img/blog/blog03.jpg" alt="blog img" width="503" height="346">
+            </div>
+            <div class="article_slot--title">
+                <p>Początek roku szkolnego – okazja na promocję Twojej firmy!</p>
+            </div>
+            <div class="article_slot--text">
+                <p>04-10-2021 | Artykuły | <a href="">Promoteca</a></p>
+            </div>
+            <div class="article_slot--button">
+                <a href="#">Więcej</a>
+            </div>
+        </div>
     </div>
 </section>
