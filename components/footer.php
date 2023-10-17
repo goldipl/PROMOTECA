260c2406f8d88494dd6c4610d5648b5f3a0856f6
goldipl <mgodlewski92@gmail.com>
2023-10-17 15:29:07 +0200

    styling & fill footer template

--- components/footer.php
+++ components/footer.php
@@ -1,13 +1,135 @@
 <div class="footer-container">
     <div class="footer-top">
         <div class="footer-left-container">
-            <div class="footer-col"></div>
+            <div class="footer-col">
+                <div class="footer-col-slot">
+                    <img src="./assets/icons/PROMOTECA_logo.svg" alt="logo" width="210" height="36">
+                </div>
+                <div class="footer-col-slot">
+                    <div class="footer-col-slot--img">
+                        <img src="./assets/icons/footer/telephone.svg" alt="telephone" width="15" height="15">
+                    </div>
+                    <div class="footer-col-slot--text">
+                        <a href="tel:+22 486 34 83">22 486 34 83</a>
+                    </div>
+                </div>
+                <div class="footer-col-slot">
+                    <div class="footer-col-slot--img">
+                        <img src="./assets/icons/footer/clock.svg" alt="clock" width="15" height="15">
+                    </div>
+                    <div class="footer-col-slot--text">
+                        <p>9:00 - 17:00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-col-slot">
+                <div class="footer-col-slot--img">
+                    <img src="./assets/icons/footer/email.svg" alt="email" width="15" height="15">
+                </div>
+                <div class="footer-col-slot--text">
+                    <a href="mailto:Info@promoteca.pl">Info@promoteca.pl</a>
+                </div>
+            </div>
         </div>
         <div class="footer-right-container">
-            <div class="footer-col"></div>
+            <div class="footer-col">
+                <div class="footer-col--title">
+                    <p>Pomoc</p>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Zwroty i reklamacje</a>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Regulamin</a>
+                </div>
+            </div>
+            <div class="footer-col">
+                <div class="footer-col--title">
+                    <p>Moje konto</p>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Twoje zamówienia</a>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Ustawienia konta</a>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Przechowalnia</a>
+                </div>
+            </div>
+            <div class="footer-col">
+                <div class="footer-col--title">
+                    <p>Płatności i dostawa</p>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Formy płatności</a>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Czas i koszty dostawy</a>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Czas realizacji zamówienia</a>
+                </div>
+            </div>
+            <div class="footer-col">
+                <div class="footer-col--title">
+                    <p>Informacje</p>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Polityka prywatności</a>
+                </div>
+            </div>
+            <div class="footer-col">
+                <div class="footer-col--title">
+                    <p>O nas</p>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">Kontakt</a>
+                </div>
+                <div class="footer-col--link">
+                    <a href="">O firmie</a>
+                </div>
+            </div>
         </div>
     </div>
     <div class="footer-bottom">
-        <p>© <span></span> PROMOTECA. Wszystkie prawa zastrzeżone </p>
+        <div class="footer-three-cols">
+            <div class="footer-col">
+                <p>© <span></span> Promoteca</p>
+            </div>
+            <div class="footer-col">
+                <div class="footer-col_slot">
+                    <div class="footer-col_slot--img">
+                        <img src="./assets/icons/footer/location.svg" alt="location" width="12" height="15">
+                    </div>
+                    <div class="footer-col_slot--text">
+                        <p>Exito Group</p>
+                    </div>
+                </div>
+                <div class="footer-col_line"></div>
+                <div class="footer-col_slot">
+                    <div class="footer-col_slot--text">
+                        <p>ul. Baletowa 12</p>
+                    </div>
+                </div>
+                <div class="footer-col_line"></div>
+                <div class="footer-col_slot">
+                    <div class="footer-col_slot--text">
+                        <p>02-862 Warszawa</p>
+                    </div>
+                </div>
+                <div class="footer-col_line"></div>
+                <div class="footer-col_slot">
+                    <div class="footer-col_slot--text">
+                        <p>NIP: 5213362020</p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-col">
+                <a href="https://www.facebook.com/PromotecaPL/">
+                    <img src="./assets/icons/footer/fb.svg" alt="fb" width="7" height="15">
+                </a>
+            </div>
+        </div>
     </div>
 </div>
