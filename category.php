98c920f2abc11e1e2c9f8e8cfa4e94c81540ba1b
goldipl <mgodlewski92@gmail.com>
2023-10-18 08:31:50 +0200

    add category page

---
+++ category.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PROMOTECA | Strona kategorii</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body class="category">
+        <header> 
+            <?php include "./components/topbar.php"; ?> 
+            <?php include "./components/nav.php"; ?> 
+        </header>
+        <div id="main-wrapper">
+
+        </div>
+        <footer> 
+            <?php include "./components/footer.php"; ?> 
+        </footer>
+        <script src="./js/mobile_searchbar.js"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
