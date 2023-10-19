960e807b2ca6b031be5a70e3c81bd0fe832046c6
goldipl <mgodlewski92@gmail.com>
2023-10-19 16:43:25 +0200

    correct data indexes

--- components/products_card/products_card_gallery_open.php
+++ components/products_card/products_card_gallery_open.php
@@ -2,21 +2,21 @@
     <div class="products_card_gallery_open--gallery">
         <div class="gallery-container">
             <div class="gallery-container-big">
-                <div class="gallery-item big" data-index="5">
+                <div class="gallery-item big" data-index="1">
                     <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
                 </div>
             </div>
             <div class="gallery-container-small">
-                <div class="gallery-item small" data-index="1">
-                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
-                </div>
                 <div class="gallery-item small" data-index="2">
-                    <img src="./assets/img/recommend/document_clip.jpg">
+                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
                 </div>
                 <div class="gallery-item small" data-index="3">
-                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
+                    <img src="./assets/img/recommend/document_clip.jpg">
                 </div>
                 <div class="gallery-item small" data-index="4">
+                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
+                </div>
+                <div class="gallery-item small" data-index="5">
                     <img src="./assets/img/recommend/document_clip.jpg">
                 </div>
             </div>
