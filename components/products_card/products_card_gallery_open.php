843b6c4ec10837897b46b60ed1b39d0826461e1e
goldipl <mgodlewski92@gmail.com>
2023-10-19 12:57:47 +0200

    add products gallery & pre styles

---
+++ components/products_card/products_card_gallery_open.php
@@ -0,0 +1,28 @@
+<div class="products_card_gallery_open">
+    <div class="products_card_gallery_open--gallery">
+        <div class="gallery-container">
+            <div class="gallery-container-big">
+                <div class="gallery-item big" data-index="5">
+                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
+                </div>
+            </div>
+            <div class="gallery-container-small">
+                <div class="gallery-item small" data-index="1">
+                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
+                </div>
+                <div class="gallery-item small" data-index="2">
+                    <img src="./assets/img/recommend/document_clip.jpg">
+                </div>
+                <div class="gallery-item small" data-index="3">
+                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg">
+                </div>
+                <div class="gallery-item small" data-index="4">
+                    <img src="./assets/img/recommend/document_clip.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="products_card_gallery_open--details">
+
+    </div>
+</div>
