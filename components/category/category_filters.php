ec18cb768d8067e06765cea90f53635e7fb464b6
goldipl <mgodlewski92@gmail.com>
2023-10-19 08:25:10 +0200

    styling cataegory filters

--- components/category/category_filters.php
+++ components/category/category_filters.php
@@ -4,23 +4,23 @@
         <ul class="category_filters-slot--list producer">
             <li>
                 <input type="checkbox" id="tag01" name="tag01" value="Tag 01">
-                <label for="tag01">Tag 01 <p>(<span>457</span>)</p></label>
+                <label for="tag01">Air Gifts <p>(<span>457</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="tag02" name="tag02" value="Tag 02">
-                <label for="tag02">Tag 02 <p>(<span>4</span>)</p></label>
+                <label for="tag02">AdPen <p>(<span>4</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="tag03" name="tag03" value="Tag 03">
-                <label for="tag03">Tag 03 <p>(<span>4</span>)</p></label>
+                <label for="tag03">AdSmycze <p>(<span>4</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="tag04" name="tag04" value="Tag 04">
-                <label for="tag04">Tag 04 <p>(<span>4</span>)</p></label>
+                <label for="tag04">AXPOL <p>(<span>4</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="tag05" name="tag05" value="Tag 05">
-                <label for="tag05">Tag 05 <p>(<span>4</span>)</p></label>
+                <label for="tag05">Lecce Pen <p>(<span>4</span>)</p></label>
             </li>
         </ul>
         <div class="filters-btns">
@@ -38,23 +38,23 @@
         <ul class="category_filters-slot--list color">
             <li>
                 <input type="checkbox" id="color01" name="color01" value="color 01">
-                <label for="color01">Kolor 01 <p>(<span>457</span>)</p></label>
+                <label for="color01"><div class="color_square white"></div>Biały <p>(<span>457</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="color02" name="color02" value="color 02">
-                <label for="color02">Kolor 02 <p>(<span>457</span>)</p></label>
+                <label for="color02"><div class="color_square darkblue"></div>Granatowy <p>(<span>457</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="color03" name="color03" value="color 03">
-                <label for="color03">Kolor 03 <p>(<span>4</span>)</p></label>
+                <label for="color03"><div class="color_square red"></div>Czerwony <p>(<span>4</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="color04" name="color04" value="color 04">
-                <label for="color04">Kolor 04 <p>(<span>4</span>)</p></label>
+                <label for="color04"><div class="color_square yellow"></div>Żółty <p>(<span>4</span>)</p></label>
             </li>
             <li>
                 <input type="checkbox" id="color05" name="color05" value="color 05">
-                <label for="color05">Kolor 05 <p>(<span>4</span>)</p></label>
+                <label for="color05"><div class="color_square green"></div>Zielony <p>(<span>4</span>)</p></label>
             </li>
         </ul>
         <div class="filters-btns">
