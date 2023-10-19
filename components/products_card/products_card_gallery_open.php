baa545b5947b5ffff068ff14ae427cc37c433654
goldipl <mgodlewski92@gmail.com>
2023-10-19 16:18:23 +0200

    fill template with icons

--- components/products_card/products_card_gallery_open.php
+++ components/products_card/products_card_gallery_open.php
@@ -26,40 +26,56 @@
         <h1 class="title">Klip do dokumentów V2794-08</h1>
         <div class="details_desc_container">
             <div class="details_desc_container-column">
-                <div class="details_desc_container-columm--logo">
-                    <img src="./assets/img/products_card/axpol_logo.jpg" alt="logo" width="113" height="113">
+                <div class="details_desc_container-column--logo">
+                    <img src="./assets/img/products_card/axpol_logo.jpg" alt="logo" width="113" height="64">
                 </div>
-                <div class="details_desc_container-columm--text">
+                <div class="details_desc_container-column--text">
                     <p>Producent: AXPOL</p>
                 </div>
-                <div class="details_desc_container-columm--rating">
-                    <p>Ocena: <img src="./assets/icons/products_card/stars4.svg" alt="rating"></p>
+                <div class="details_desc_container-column--rating">
+                    <p>Ocena: <img src="./assets/icons/products_card/stars4.svg" alt="rating">
+                    </p>
                 </div>
-                <div class="details_desc_container-columm--text">
-                    <p>Kod produktu: V0261-16</p>
+                <div class="details_desc_container-column--text">
+                    <p>Kod produktu: <span>V0261-16</span></p>
                 </div>
             </div>
             <div class="details_desc_container-column">
-                <div class="details_desc_container-columm--text">
-                    <p><span class="green-text">Dostępny: </span>8500 sztuk</p>
+                <div class="details_desc_container-column--text available">
+                    <p>
+                        <span class="green-text">Dostępny: </span>8500 sztuk
+                    </p>
                 </div>
-                <div class="details_desc_container-columm--text grey">
-                    <p>Zapytaj o produkt</p>
+                <div class="details_desc_container-column--text grey">
+                    <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" viewBox="0 0 15 12" fill="none">
+                            <path d="M0 1.875C0 1.37772 0.197544 0.900805 0.549175 0.549175C0.900805 0.197544 1.37772 0 1.875 0H13.125C13.6223 0 14.0992 0.197544 14.4508 0.549175C14.8025 0.900805 15 1.37772 15 1.875V9.375C15 9.87228 14.8025 10.3492 14.4508 10.7008C14.0992 11.0525 13.6223 11.25 13.125 11.25H1.875C1.37772 11.25 0.900805 11.0525 0.549175 10.7008C0.197544 10.3492 0 9.87228 0 9.375V1.875ZM1.875 0.9375C1.62636 0.9375 1.3879 1.03627 1.21209 1.21209C1.03627 1.3879 0.9375 1.62636 0.9375 1.875V2.07844L7.5 6.01594L14.0625 2.07844V1.875C14.0625 1.62636 13.9637 1.3879 13.7879 1.21209C13.6121 1.03627 13.3736 0.9375 13.125 0.9375H1.875ZM14.0625 3.17156L9.64875 5.82L14.0625 8.53594V3.17156ZM14.0306 9.61781L8.74312 6.36375L7.5 7.10906L6.25688 6.36375L0.969375 9.61688C1.02265 9.81637 1.1403 9.9927 1.30405 10.1185C1.46781 10.2443 1.66851 10.3125 1.875 10.3125H13.125C13.3314 10.3125 13.532 10.2445 13.6957 10.1189C13.8595 9.99326 13.9772 9.81714 14.0306 9.61781ZM0.9375 8.53594L5.35125 5.82L0.9375 3.17156V8.53594Z" fill="#ADADAD" />
+                        </svg> Zapytaj o produkt
+                    </p>
                 </div>
-                <div class="details_desc_container-columm--text grey">
-                    <p>Napisz ocenę</p>
+                <div class="details_desc_container-column--text grey">
+                    <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+                            <path d="M7.33333 2.47712L10 5.19794M6.66667 12H12M1 11.6599L1.33333 8.59897L8.39052 1.39846C8.91122 0.867182 9.75544 0.867182 10.2761 1.39846L11.0572 2.19537C11.5779 2.72664 11.5779 3.58801 11.0572 4.11928L4 11.3198L1 11.6599Z" stroke="#ADADAD" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg> Napisz ocenę
+                    </p>
                 </div>
-                <div class="details_desc_container-columm--text grey">
-                    <p>Poleć znajomemu</p>
+                <div class="details_desc_container-column--text grey">
+                    <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path d="M7 13.125C5.37555 13.125 3.81763 12.4797 2.66897 11.331C1.52031 10.1824 0.875 8.62445 0.875 7C0.875 5.37555 1.52031 3.81763 2.66897 2.66897C3.81763 1.52031 5.37555 0.875 7 0.875C8.62445 0.875 10.1824 1.52031 11.331 2.66897C12.4797 3.81763 13.125 5.37555 13.125 7C13.125 8.62445 12.4797 10.1824 11.331 11.331C10.1824 12.4797 8.62445 13.125 7 13.125ZM7 14C8.85652 14 10.637 13.2625 11.9497 11.9497C13.2625 10.637 14 8.85652 14 7C14 5.14348 13.2625 3.36301 11.9497 2.05025C10.637 0.737498 8.85652 0 7 0C5.14348 0 3.36301 0.737498 2.05025 2.05025C0.737498 3.36301 0 5.14348 0 7C0 8.85652 0.737498 10.637 2.05025 11.9497C3.36301 13.2625 5.14348 14 7 14Z" fill="#ADADAD" />
+                            <path d="M7 3.5C7.11603 3.5 7.22731 3.54609 7.30936 3.62814C7.39141 3.71019 7.4375 3.82147 7.4375 3.9375V6.5625H10.0625C10.1785 6.5625 10.2898 6.60859 10.3719 6.69064C10.4539 6.77269 10.5 6.88397 10.5 7C10.5 7.11603 10.4539 7.22731 10.3719 7.30936C10.2898 7.39141 10.1785 7.4375 10.0625 7.4375H7.4375V10.0625C7.4375 10.1785 7.39141 10.2898 7.30936 10.3719C7.22731 10.4539 7.11603 10.5 7 10.5C6.88397 10.5 6.77269 10.4539 6.69064 10.3719C6.60859 10.2898 6.5625 10.1785 6.5625 10.0625V7.4375H3.9375C3.82147 7.4375 3.71019 7.39141 3.62814 7.30936C3.54609 7.22731 3.5 7.11603 3.5 7C3.5 6.88397 3.54609 6.77269 3.62814 6.69064C3.71019 6.60859 3.82147 6.5625 3.9375 6.5625H6.5625V3.9375C6.5625 3.82147 6.60859 3.71019 6.69064 3.62814C6.77269 3.54609 6.88397 3.5 7 3.5Z" fill="#ADADAD" />
+                        </svg> Poleć znajomemu
+                    </p>
                 </div>
             </div>
         </div>
         <div class="price_cart_btn__container">
             <div class="price_cart_btn__container--price">
-                <p>Cena od <span class="blue-price">0.90</span> zł</p>
+                <p>Cena od <span class="blue-price">0.90</span> zł </p>
             </div>
             <div class="price_cart_btn__container--input">
-                <input type="number" name="products_quantity" id="products_quantity" value="1">
+                <input type="number" name="products_quantity" id="products_quantity" value="1" min="0">
             </div>
             <div class="price_cart_btn__container--btn">
                 <a href="#">Do koszyka</a>
@@ -67,7 +83,7 @@
         </div>
         <div class="colors_container">
             <div class="actual_color">
-                <strong>Kolor:</strong> 
+                <strong>Kolor:</strong>
                 <div class="color_square yellow"></div>
                 <span class="color_name">Żółty</span>
             </div>
