2e8d4db5d731b2e86daa86e28dd18023e6a4758f
goldipl <mgodlewski92@gmail.com>
2023-10-19 15:11:20 +0200

    fill products card gallery open template

--- components/products_card/products_card_gallery_open.php
+++ components/products_card/products_card_gallery_open.php
@@ -26,10 +26,60 @@
         <h1 class="title">Klip do dokumentów V2794-08</h1>
         <div class="details_desc_container">
             <div class="details_desc_container-column">
-
+                <div class="details_desc_container-columm--logo">
+                    <img src="./assets/img/products_card/axpol_logo.jpg" alt="logo" width="113" height="113">
+                </div>
+                <div class="details_desc_container-columm--text">
+                    <p>Producent: AXPOL</p>
+                </div>
+                <div class="details_desc_container-columm--rating">
+                    <p>Ocena: <img src="./assets/icons/products_card/stars4.svg" alt="rating"></p>
+                </div>
+                <div class="details_desc_container-columm--text">
+                    <p>Kod produktu: V0261-16</p>
+                </div>
             </div>
             <div class="details_desc_container-column">
-                
+                <div class="details_desc_container-columm--text">
+                    <p><span class="green-text">Dostępny: </span>8500 sztuk</p>
+                </div>
+                <div class="details_desc_container-columm--text grey">
+                    <p>Zapytaj o produkt</p>
+                </div>
+                <div class="details_desc_container-columm--text grey">
+                    <p>Napisz ocenę</p>
+                </div>
+                <div class="details_desc_container-columm--text grey">
+                    <p>Poleć znajomemu</p>
+                </div>
+            </div>
+        </div>
+        <div class="price_cart_btn__container">
+            <div class="price_cart_btn__container--price">
+                <p>Cena od <span class="blue-price">0.90</span> zł</p>
+            </div>
+            <div class="price_cart_btn__container--input">
+                <input type="number" name="products_quantity" id="products_quantity" value="1">
+            </div>
+            <div class="price_cart_btn__container--btn">
+                <a href="#">Do koszyka</a>
+            </div>
+        </div>
+        <div class="colors_container">
+            <div class="actual_color">
+                <strong>Kolor:</strong> 
+                <div class="color_square yellow"></div>
+                <span class="color_name">Żółty</span>
+            </div>
+            <div class="colors_options">
+                <div class="color_square white"></div>
+                <div class="color_square darkblue"></div>
+                <div class="color_square red"></div>
+                <div class="color_square yellow"></div>
+                <div class="color_square green"></div>
+                <div class="color_square purple"></div>
+                <div class="color_square brown"></div>
+                <div class="color_square yellow2"></div>
             </div>
         </div>
     </div>
