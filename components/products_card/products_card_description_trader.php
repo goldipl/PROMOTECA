0201c9be694bc4638d46dda87bf3c0bc7de611f7
goldipl <mgodlewski92@gmail.com>
2023-10-22 23:00:17 +0200

    add producsts card trader & styles

---
+++ components/products_card/products_card_description_trader.php
@@ -0,0 +1,11 @@
+<div class="products_card_description_trader">
+    <div class="desc_container">
+        <div class="desc_title_box">
+            <h2 class="desc_title_box--title">Stany magazynowe</h2>
+        </div>
+        <div class="desc_title_box_content">
+            <p><span class="bold_green">Dostępny: </span>8500 sztuk</p>
+            <p>na zamówienie w ciągu 7-10 dni</p>
+        </div>
+    </div>
+</div>
---
+++ products_card_trader.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PROMOTECA | Karta produktu</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body class="products_card_page">
+        <header> 
+            <?php include "./components/topbar.php"; ?> 
+            <?php include "./components/nav.php"; ?> 
+        </header>
+        <div id="main-wrapper">
+            <div class="breadcrumbs">
+                <ul>
+                    <li class="breadcrumb-item">
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="#">Gadżety biurowe</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="#">Akcesoria biurowe</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="#">Klip do dokumentów V2794-08</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="row__container">
+                <aside>
+                    <?php include "./components/common/aside_menu.php"; ?> 
+                    <?php include "./components/common/aside_boxes.php"; ?> 
+                </aside>
+                <div class="col_10__container">
+                    <?php include "./components/products_card/products_card_gallery_open.php"; ?>
+                    <?php include "./components/products_card/products_card_description_trader.php"; ?>
+                    <?php include "./components/products_card/products_card_recommended.php"; ?>
+                </div>
+            </div>
+        </div>
+        <footer> 
+            <?php include "./components/footer.php"; ?> 
+        </footer>
+        <script src="./js/mobile_searchbar.js"></script>
+        <script src="./js/script.js"></script>
+        <script src="./js/accordion_title.js"></script>
+        <script src="./js/products_page/products_gallery.js"></script>
+    </body>
+</html>
