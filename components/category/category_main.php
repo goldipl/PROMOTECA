5210984e1a82c2a993d35da302b08e813f71459b
goldipl <mgodlewski92@gmail.com>
2023-10-23 20:50:43 +0200

    styling labels

--- components/category/category_main.php
+++ components/category/category_main.php
@@ -5,7 +5,7 @@
     <?php include "./components/category/category_top_options.php"; ?> 
 
     <div class="products_blocks_wrapper">
-        <div class="products_blocks_wrapper__slot">
+        <div class="products_blocks_wrapper__slot new">
             <div class="products_block_wrapper__slot--img">
                 <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
             </div>
@@ -23,7 +23,7 @@
             </div>
         </div>
 
-        <div class="products_blocks_wrapper__slot">
+        <div class="products_blocks_wrapper__slot promotion">
             <div class="products_block_wrapper__slot--img">
                 <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
             </div>
@@ -41,7 +41,7 @@
             </div>
         </div>
 
-        <div class="products_blocks_wrapper__slot">
+        <div class="products_blocks_wrapper__slot recommended">
             <div class="products_block_wrapper__slot--img">
                 <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
             </div>
