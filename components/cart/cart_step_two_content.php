7355403cbcc34d725a711c0266e743d3f33c96bd
goldipl <mgodlewski92@gmail.com>
2023-10-24 13:23:27 +0200

    add cart step two template & styles
    - fill cart_step_two_content.php template with all tags
    - styling step two template
    - seperate carts pay box styles from cart_step_two.scss file

--- components/cart/cart_step_two_content.php
+++ components/cart/cart_step_two_content.php
@@ -1,3 +1,48 @@
 <div class="cart_step_two_container">
-
+    <div class="cart_login_container">
+        <div class="cart_login_container-top">
+            <div class="cart_login_container-top__left">
+                <p class="title">Zaloguj się</p>
+                <div class="input-slot">
+                    <label for="email_login">Adres e-mail:</label>
+                    <input type="email" name="email_login" id="email_login" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+                </div>
+                <div class="input-slot">
+                    <label for="password_login">Hasło:</label>
+                    <input type="password" name="password_login" id="password_login" required>
+                </div>
+                <div class="input-slot">
+                    <a class="remind_password" href="">Zapomniałeś hasło?</a>
+                </div>
+                <div class="input-slot">
+                    <div class="blue-button login">
+                        <a href="">Zaloguj się</a>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_login_container-top__right">
+                <div class="cart_login_container-top__right--slot">
+                    <p class="title">Nie posiadasz jeszcze konta?</p>
+                    <p class="text">Zarejestruj się na promoteca.pl</p>
+                    <div class="input-slot">
+                        <div class="blue-button create-account">
+                            <a href="">Utwórz konto</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart_login_container-top__right--slot">
+                    <p class="title">Jeśli nie chcesz zakładać konta? Nic straconego</p>
+                    <p class="text">Możesz zrobić zakupy bez rejesrtacji</p>
+                    <div class="input-slot">
+                        <div class="blue-button fast-shopping">
+                            <a href="">Szybkie zakupy</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cart_login_container__button">
+            <a href="">Wstecz</a>
+        </div>
+    </div>
 </div>
