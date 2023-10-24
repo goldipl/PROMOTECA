13edd8b54a6fb08ffa2369b5d885aaf74184163c
goldipl <mgodlewski92@gmail.com>
2023-10-24 15:58:20 +0200

    add body classes

--- components/cart/cart_step_two_content_without_account.php
+++ components/cart/cart_step_two_content_without_account.php
@@ -1,3 +1,111 @@
 <div class="cart_step_two_without_account_container">
-
+    <div class="cart_step_two_without_account_container--left">
+        <div class="cart_without_account__box">
+            <div class="cart_without_account__box--left">
+                <p class="title">Dane osobowe</p>
+                <div class="input-slot">
+                    <label for="email_login">Adres e-mail: *</label>
+                    <input type="email" name="email_login" id="email_login" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+                </div>
+                <div class="input-slot">
+                    <label for="password_login">Hasło: *</label>
+                    <input type="password" name="password_login" id="password_login" required>
+                </div>
+                <div class="input-slot">
+                    <label for="repeat_password_login">Powtórz hasło: *</label>
+                    <input type="password" name="repeat_password_login" id="repeat_password_login" required>
+                </div>
+            </div>
+            <div class="cart_without_account__box--right">
+            <p class="title">Dane do faktury</p>
+                <div class="input-slot">
+                    <label for="company_name_surname">Nazwa firmy / Imię i Nazwisko:</label>
+                    <input type="text" name="company_name_surname" id="company_name_surname">
+                </div>
+                <div class="input-slot with-btn">
+                    <label for="nip_code">NIP:</label>
+                    <input type="text" name="nip_code" id="nip_code">
+                    <div class="gus_btn">
+                        <a href="">Pobierz dane z GUS</a>
+                    </div>
+                </div>
+                <div class="input-slot">
+                    <label for="address">Adres:</label>
+                    <input type="text" name="address" id="address">
+                </div>
+                <div class="input-slot">
+                    <label for="zipcode">Kod pocztowy:</label>
+                    <input type="text" name="zipcode" id="zipcode">
+                </div>
+                <div class="input-slot">
+                    <label for="city">Miasto:</label>
+                    <input type="text" name="city" id="city">
+                </div>
+                <div class="input-slot checkbox-slot">
+                    <input type="checkbox" name="different_address" id="different_address" required>
+                    <label class="checkbox" for="different_address">Adres inny niż na zamówieniu</label>
+                </div>
+                <div class="input-slot checkbox-slot">
+                    <input type="checkbox" name="register_checkbox" id="register_checkbox" required>
+                    <label class="checkbox" for="register_checkbox">Zapoznałem się i akceptuję Regulamin oraz Politykę Prywatności</label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cart_step_two_without_account_container--right">
+        <div class="cart_pay_box">
+            <div class="cart_pay_box__top">
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Wartość produktów</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>47,00 zł</p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Wartość znakowania</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>50,00 zł</p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Dostawa:</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>20,00 zł</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_pay_box__bottom">
+                <div class="cart_pay_box__bottom-slot">
+                    <div class="cart_pay_box__bottom-slot--left">
+                        <p>Całkowita cena netto:</p>
+                    </div>
+                    <div class="cart_pay_box__bottom-slot--right">
+                        <p>
+                            <span>200.90</span> zł
+                        </p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__bottom-slot">
+                    <div class="cart_pay_box__bottom-slot--left">
+                        <p>Brutto:</p>
+                    </div>
+                    <div class="cart_pay_box__bottom-slot--right">
+                        <p>220,00 zł</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_pay_box__button">
+                <a class="pay-btn" href="">Płacę</a>
+            </div>
+        </div>
+    </div>
+    <div class="cart_step_two_without_account_container--button">
+        <a href="">Wstecz</a>
+    </div>
 </div>
