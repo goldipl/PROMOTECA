20636817dc055a334d4681c91395afdfd86473aa
goldipl <mgodlewski92@gmail.com>
2023-10-25 10:01:41 +0200

    add cart step three template

--- components/cart/cart_step_three_content.php
+++ components/cart/cart_step_three_content.php
@@ -1,3 +1,110 @@
 <div class="cart_step_three_container">
-
+    <div class="cart_step_three_container__left">
+        <div class="cart_product_container">
+            <div class="cart_product_container--img">
+                <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="product img" width="140" height="140">
+            </div>
+            <div class="cart_product_container--text">
+                <div class="title">
+                    <p>Klip do dokumentów V2794-08</p>
+                </div>
+                <div class="text-slot">
+                    <div class="text-slot--left">
+                        <p>Technika znakowania:</p>
+                    </div>
+                    <div class="text-slot--right">
+                        <p>Tampodruk</p>
+                    </div>
+                </div>
+                <div class="text-slot">
+                    <div class="text-slot--left">
+                        <p>Miejsce znakowań:</p>
+                    </div>
+                    <div class="text-slot--right">
+                        <p>Z jednej strony</p>
+                    </div>
+                </div>
+                <div class="text-slot">
+                    <div class="text-slot--left">
+                        <p>Przewidywana ilość kolorów nadruku:</p>
+                    </div>
+                    <div class="text-slot--right">
+                        <p>2 kolory</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_product_container--input">
+                <label for="cart_product_quantity">Ilość produktów</label>
+                <input type="number" name="cart_product_quantity" id="cart_product_quantity" min="0">
+            </div>
+        </div>
+        <div class="cart_delivery_data_container">
+            <div class="cart_delivery_data_container__slot">
+                <p class="title">Adres dostawy</p>
+                <p class="text">Firma1 Mirosław Kozłowski</p>
+                <p class="text">Krakowska, 4c/14</p>
+                <p class="text">35-582 Kraków</p>
+            </div>
+            <div class="cart_delivery_data_container__slot">
+                <p class="title">Dane do faktury</p>
+                <p class="text">Firma1 Mirosław Kozłowski</p>
+                <p class="text">Krakowska, 4c/14</p>
+                <p class="text">35-582 Kraków</p>
+                <p class="text">NIP: 512 78541 568</p>
+            </div>
+        </div>
+    </div>
+    <div class="cart_step_three_container__right">
+        <div class="cart_pay_box">
+            <div class="cart_pay_box__top">
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Wartość produktów</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>47,00 zł</p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Wartość znakowania</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>50,00 zł</p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Dostawa:</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>20,00 zł</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_pay_box__bottom">
+                <div class="cart_pay_box__bottom-slot">
+                    <div class="cart_pay_box__bottom-slot--left">
+                        <p>Całkowita cena netto:</p>
+                    </div>
+                    <div class="cart_pay_box__bottom-slot--right">
+                        <p>
+                            <span>200.90</span> zł
+                        </p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__bottom-slot">
+                    <div class="cart_pay_box__bottom-slot--left">
+                        <p>Brutto:</p>
+                    </div>
+                    <div class="cart_pay_box__bottom-slot--right">
+                        <p>220,00 zł</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_pay_box__button">
+                <a class="pay-btn" href="">Płacę</a>
+            </div>
+        </div>
+    </div>
 </div>
