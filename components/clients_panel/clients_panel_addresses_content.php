d36bbca93312061f31f14c32a49d042eacedad87
goldipl <mgodlewski92@gmail.com>
2023-10-25 10:58:20 +0200

    fill aside clients panel

--- components/clients_panel/clients_panel_addresses_content.php
+++ components/clients_panel/clients_panel_addresses_content.php
@@ -1,3 +1,23 @@
 <div class="clients_panel_addresses_content">
+    <h1 class="title">Panel klienta</h1>
+    <div class="clients_panel_addresses_content__left">
+        <aside class="clients_panel_aside">
+            <p class="clients_panel_aside--title">Witamy!</p>
+            <a class="clients_panel_aside--slot data" href="./clients_panel_my_data.php">
+                <p>Moje Dane</p>
+            </a>
+            <a class="clients_panel_aside--slot orders" href="./clients_panel_orders.php">
+                <p>Zamówienia</p>
+            </a>
+            <a class="clients_panel_aside--slot addresses active" href="./clients_panel_addresses.php">
+                <p>Adresy</p>
+            </a>
+            <a class="clients_panel_aside--slot fv" href="./clients_panel_fv.php">
+                <p>Dane do FV</p>
+            </a>
+        </aside>
+    </div>
+    <div class="clients_panel_addresses_content__right">
 
+    </div>
 </div>
