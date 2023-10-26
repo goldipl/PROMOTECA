627306a8c9eb5a19ab690a594ed2b1a11be955fa
goldipl <mgodlewski92@gmail.com>
2023-10-26 10:45:09 +0200

    add clieant panel orders details template

--- components/clients_panel/clients_panel_orders_details_content.php
+++ components/clients_panel/clients_panel_orders_details_content.php
@@ -18,6 +18,75 @@
         </aside>
     </div>
     <div class="clients_panel_orders_details_content__right">
-
+        <div class="orders_details_box">
+            <div class="orders_details_box__slot">
+                <div class="orders_details_box__slot__top">
+                    <div class="slot-text">
+                        <p>Nr zamówienia: <strong>556451</strong></p>
+                        <p>Data zamówienia: <strong>08 września 2022</strong></p>
+                        <p>Status: <strong class="green">Zakończone</strong></p>
+                    </div>
+                </div>
+                <div class="orders_details_box__slot__bottom">
+                    <div class="slot-column">
+                        <div class="title">
+                            <p>Klip do dokumentów V2794-08</p>
+                        </div>
+                        <div class="text-slot">
+                            <div class="text-slot--left">
+                                <p>Technika znakowania:</p>
+                            </div>
+                            <div class="text-slot--right">
+                                <p>Tampodruk</p>
+                            </div>
+                        </div>
+                        <div class="text-slot">
+                            <div class="text-slot--left">
+                                <p>Miejsce znakowań:</p>
+                            </div>
+                            <div class="text-slot--right">
+                                <p>Z jednej strony</p>
+                            </div>
+                        </div>
+                        <div class="text-slot">
+                            <div class="text-slot--left">
+                                <p>Przewidywana ilość kolorów nadruku:</p>
+                            </div>
+                            <div class="text-slot--right">
+                                <p>2 kolory</p>
+                            </div>
+                        </div>
+                        <div class="address__slot">
+                            <p class="title">Adres dostawy</p>
+                            <div class="text-slot--left">
+                                <p>Firma1 Mirosław Kozłowski</p>
+                            </div>
+                            <div class="text-slot--left">
+                                <p>Krakowska, 4c/14</p>
+                            </div>
+                            <div class="text-slot--left">
+                                <p>35-582 Kraków</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slot-column">
+                        <div class="text-slot-middle">
+                            <p>Ilość produktów: <span>200</span></p>
+                        </div>
+                        <div class="text-slot-middle">
+                            <p>Zapłacono: <strong><span>200.90</span> zł</strong></p>
+                        </div>
+                    </div>
+                    <div class="slot-column">
+                        <div class="image">
+                            <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="img" width="50" height ="50">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clients_panel_orders_details_content--button_back">
+            <a href="./clients_panel_orders.php">Wstecz</a>
+        </div>
     </div>
 </div>
