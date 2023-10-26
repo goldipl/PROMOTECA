a23c01d414e325fc7a9cfd53fbb85cfe591f6183
goldipl <mgodlewski92@gmail.com>
2023-10-26 08:17:39 +0200

    add clients panel orders template

--- components/clients_panel/clients_panel_orders_content.php
+++ components/clients_panel/clients_panel_orders_content.php
@@ -20,7 +20,62 @@
     <div class="clients_panel_orders_content__right">
         <div class="panel_orders_box">
             <div class="panel_orders_box__slot">
-
+                <div class="panel_orders_box__slot--col">
+                    <div class="slot-text">
+                        <p>Data zamówienia: <strong>08 września 2022</strong></p>
+                        <p>Nr zamówienia: <strong>556451</strong></p>
+                        <p>Status: <strong class="green">Zakończone</strong></p>
+                    </div>
+                </div>
+                <div class="panel_orders_box__slot--col">
+                    <div class="col-first-line">
+                        <div class="slot-img">
+                            <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="img" width="50" height ="50">
+                        </div>
+                        <div class="slot-title">
+                            <strong>Klip do dokumentów V2794-08</strong>
+                        </div>
+                    </div>
+                    <div class="col-second-line">
+                        <div class="pay-value">
+                            <p>Zapłacaono: <strong><span class="blue">200.90</span> zł</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel_orders_box__slot--col">
+                    <div class="details-btn">
+                        <a href="./clients_panel_orders_details_content.php">Szczegóły</a>
+                    </div>
+                </div>
+            </div>
+            <div class="panel_orders_box__slot">
+                <div class="panel_orders_box__slot--col">
+                    <div class="slot-text">
+                        <p>Data zamówienia: <strong>08 września 2022</strong></p>
+                        <p>Nr zamówienia: <strong>556451</strong></p>
+                        <p>Status: <strong class="orange">W realizacji</strong></p>
+                    </div>
+                </div>
+                <div class="panel_orders_box__slot--col">
+                    <div class="col-first-line">
+                        <div class="slot-img">
+                            <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="img" width="50" height ="50">
+                        </div>
+                        <div class="slot-title">
+                            <strong>Klip do dokumentów V2794-08</strong>
+                        </div>
+                    </div>
+                    <div class="col-second-line">
+                        <div class="pay-value">
+                            <p>Zapłacaono: <strong><span class="blue">200.90</span> zł</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel_orders_box__slot--col">
+                    <div class="details-btn">
+                        <a href="./clients_panel_orders_details_content.php">Szczegóły</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
