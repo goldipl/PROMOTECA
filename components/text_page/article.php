c04f3dc5ceb4ade4d66c9ee12e09ee0be881261e
goldipl <mgodlewski92@gmail.com>
2023-11-15 16:34:04 +0100

    styling article container in one wrapper

--- components/text_page/article.php
+++ components/text_page/article.php
@@ -1,6 +1,5 @@
+<h1 class="article_title">Podstrona tekstowa</h1>
 <div class="article_container">
-    <h1 class="title">Podstrona tekstowa</h1>
-    <div class="article_img_txt_box">
         <div class="article_img_txt_box--img">
             <img src="./assets/img/article/article_img_01.jpg" alt="img" width="775" height="232">
         </div>
@@ -8,8 +7,6 @@
             <p class="title">Lorem ipsum</p>
             <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
         </div>
-    </div>
-    <div class="article_txt_img_box">
         <div class="article_txt_img_box--text">
             <p class="title">Lorem ipsum</p>
             <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
@@ -17,8 +14,6 @@
         <div class="article_txt_img_box--img">
             <img src="./assets/img/article/article_img_02.jpg" alt="img" width="775" height="232">
         </div>
-    </div>
-    <div class="article_small_img_box">
         <div class="article_small_img_box--img">
             <img src="./assets/img/article/article_img_small_01.jpg" alt="img" width="380" height="232">
         </div>
@@ -31,8 +26,6 @@
         <div class="article_small_img_box--img">
             <img src="./assets/img/article/article_img_small_04.jpg" alt="img" width="380" height="232">
         </div>
-    </div>
-    <div class="article_txt_img_img_box">
         <div class="article_txt_img_img_box--text">
             <p class="title">Lorem ipsum</p>
             <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh diam tellus cursus vitae mi, tempor sem. Pharetra, in risus sem urna lacus, cursus vitae est. Nunc, aliquam, dui tempus gravida. Et diam euismod mauris elit.</p>
@@ -45,5 +38,4 @@
                 <img src="./assets/img/article/article_img_04.jpg" alt="img" width="775" height="232">
             </div>
         </div>
-    </div>
 </div>
