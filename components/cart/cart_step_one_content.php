3b2d7965e369b99d517772980b922563b056936b
goldipl <mgodlewski92@gmail.com>
2023-11-19 13:50:25 +0100

    correct box styles

--- components/cart/cart_step_one_content.php
+++ components/cart/cart_step_one_content.php
@@ -1,71 +1,73 @@
 <div class="cart_step_one_container">
     <div class="cart_step_one_container__left">
-        <div class="cart_product_container">
-            <div class="cart_product_container--img">
-                <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="product img" width="140" height="140">
-            </div>
-            <div class="cart_product_container--text">
-                <div class="title">
-                    <p>Klip do dokumentów V2794-08</p>
+        <div class="cart_products_box">
+            <div class="cart_product_container">
+                <div class="cart_product_container--img">
+                    <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="product img" width="140" height="140">
                 </div>
-                <div class="text-slot">
-                    <div class="text-slot--left">
-                        <p>Technika znakowania:</p>
+                <div class="cart_product_container--text">
+                    <div class="title">
+                        <p>Klip do dokumentów V2794-08</p>
                     </div>
-                    <div class="text-slot--right">
-                        <p>Tampodruk</p>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Technika znakowania:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>Tampodruk</p>
+                        </div>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Miejsce znakowań:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>Z jednej strony</p>
+                        </div>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Przewidywana ilość kolorów nadruku:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>2 kolory</p>
+                        </div>
                     </div>
                 </div>
-                <div class="text-slot">
-                    <div class="text-slot--left">
-                        <p>Miejsce znakowań:</p>
-                    </div>
-                    <div class="text-slot--right">
-                        <p>Z jednej strony</p>
-                    </div>
-                </div>
-                <div class="text-slot">
-                    <div class="text-slot--left">
-                        <p>Przewidywana ilość kolorów nadruku:</p>
-                    </div>
-                    <div class="text-slot--right">
-                        <p>2 kolory</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cart_product_container--input">
-                <label for="cart_product_quantity">Ilość produktów</label>
-                <input type="number" name="cart_product_quantity" id="cart_product_quantity" min="0">
-                <div class="cart_product_container--delete">
-                    <p>Usuń</p>
-                </div>
-            </div>
-        </div>
-        <div class="upload_logo_notes_container">
-            <div class="upload_logo_notes_container__left">
-                <div class="title">
-                    <p>Wgraj logo lub projekt:</p>
-                </div>
-                <div class="upload-area">
-                    <div class="upload-area--img">
-                        <img src="./assets/icons/cart/upload.svg" alt="upload" width="21" height="14">
-                    </div>
-                    <div class="upload-area--text">
-                        <p>Przeciągnij i upuść albo <br>
-                            <a href="">Dodaj plik</a>
-                        </p>
-                    </div>
-                    <div class="upload-area--small-text">
-                        <p>Wymagamy plik w wektorach <br>format pliku pdf zapisany w krzywych (czcionka i kontury) </p>
+                <div class="cart_product_container--input">
+                    <label for="cart_product_quantity">Ilość produktów</label>
+                    <input type="number" name="cart_product_quantity" id="cart_product_quantity" min="0">
+                    <div class="cart_product_container--delete">
+                        <p>Usuń</p>
                     </div>
                 </div>
             </div>
-            <div class="upload_logo_notes_container__right">
-                <div class="title">
-                    <p>Zostaw notatkę:</p>
+            <div class="upload_logo_notes_container">
+                <div class="upload_logo_notes_container__left">
+                    <div class="title">
+                        <p>Wgraj logo lub projekt:</p>
+                    </div>
+                    <div class="upload-area">
+                        <div class="upload-area--img">
+                            <img src="./assets/icons/cart/upload.svg" alt="upload" width="21" height="14">
+                        </div>
+                        <div class="upload-area--text">
+                            <p>Przeciągnij i upuść albo <br>
+                                <a href="">Dodaj plik</a>
+                            </p>
+                        </div>
+                        <div class="upload-area--small-text">
+                            <p>Wymagamy plik w wektorach <br>format pliku pdf zapisany w krzywych (czcionka i kontury) </p>
+                        </div>
+                    </div>
                 </div>
-                <div class="text-area">
-                    <textarea name="cart_notes" id="cart_notes"></textarea>
+                <div class="upload_logo_notes_container__right">
+                    <div class="title">
+                        <p>Zostaw notatkę:</p>
+                    </div>
+                    <div class="text-area">
+                        <textarea name="cart_notes" id="cart_notes"></textarea>
+                    </div>
                 </div>
             </div>
         </div>
