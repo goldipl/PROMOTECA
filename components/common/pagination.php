644e486f5b15d0c35ef33e22abe23e79c6367fc7
goldipl <mgodlewski92@gmail.com>
2023-12-05 19:34:51 +0100

    add links to pagination

--- components/common/pagination.php
+++ components/common/pagination.php
@@ -1,16 +1,28 @@
 <div class="pagination">
     <div class="pagination_container">
-        <div class="element active">
+        <div class="element prev">
+            <img src="./assets/icons/pagination/pagination_right.svg" alt="prev" width="12" height="12">
+        </div>
+        <a href="#" class="element active">
             <p>1</p>
-        </div>
-        <div class="element">
+        </a>
+        <a href="#" class="element">
             <p>2</p>
-        </div>
-        <div class="element next">
+        </a>
+        <a href="#" class="element">
+            <p>3</p>
+        </a>
+        <a href="#" class="dots">
+            <p>...</p>
+        </a>
+        <a href="#" class="element">
+            <p>25</p>
+        </a>
+        <a href="#" class="element">
+            <p>26</p>
+        </a>
+        <a href="#" class="element next">
             <img src="./assets/icons/pagination/pagination_right.svg" alt="next" width="12" height="12">
-        </div>
-        <div class="element last">
-            <img src="./assets/icons/pagination/pagination_last.svg" alt="last" width="12" height="12">
-        </div>
+        </a>
     </div>
 </div>
