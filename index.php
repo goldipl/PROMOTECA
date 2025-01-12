d0efee567a98507cd12150e7fee64c8b9ac298ca
goldipl <mgodlewski92@gmail.com>
2025-01-12 13:42:47 +0100

    change recommend section to home products section

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <?php include "./components/main_page/welcome_seo_text.php"; ?> 
             <?php include "./components/main_page/swiper.php"; ?> 
             <?php include "./components/main_page/categories_swiper.php"; ?> 
-            <?php include "./components/main_page/recommend_section.php"; ?> 
+            <?php include "./components/main_page/home_products_section.php"; ?> 
             <?php include "./components/main_page/our_offert.php"; ?> 
             <?php include "./components/main_page/blog.php"; ?> 
             <?php include "./components/main_page/why_us.php"; ?> 
