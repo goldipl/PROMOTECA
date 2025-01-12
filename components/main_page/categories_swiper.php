c7c63cd0598d903c5e08955ac4bd58a658d49d89
goldipl <mgodlewski92@gmail.com>
2025-01-12 13:04:57 +0100

    add categories swiper imgs

---
+++ components/main_page/categories_swiper.php
@@ -0,0 +1,172 @@
+<section id="categories-swiper">
+    <!-- Slider main container -->
+    <div class="categories-swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/mug-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/pen-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/bag-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/book-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/box-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/joy-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/music-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/cake-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/mug-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/pen-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/bag-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/book-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/box-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/joy-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/music-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="" class="category-swiper-link">
+                    <div class="category-swiper-icon-box">
+                        <img src="./assets/icons/category_swiper/cake-icon.jpg" alt="category icon" widt="70" height="70">
+                    </div>
+                    <div class="category-swiper-text-box">
+                        <span>Nazwa kategorii</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+    </div>
+</section>
