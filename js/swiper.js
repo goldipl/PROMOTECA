2435d76276d9520ee19423d1a6650a317873cb97
goldipl <mgodlewski92@gmail.com>
2025-01-12 13:40:30 +0100

    handle and add a new products swiper

--- js/swiper.js
+++ js/swiper.js
@@ -55,3 +55,34 @@ const categoriesSwiper = new Swiper('.categories-swiper', {
     },
   },
 });
+
+const newGadgetsSwiper = new Swiper('.new-gadgets-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    // when window width is >= 1040px
+    1040: {
+      slidesPerView: 6,
+    },
+    // when window width is >= 840px
+    840: {
+      slidesPerView: 5,
+    },
+    // when window width is >= 740px
+    740: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 200px
+    200: {
+      slidesPerView: 3,
+    },
+  },
+});
