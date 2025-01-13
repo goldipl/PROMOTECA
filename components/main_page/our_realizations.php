95674e20047b6a2982fc48f567c22f1e8b7ca7b6
goldipl <mgodlewski92@gmail.com>
2025-01-13 16:51:28 +0100

    add our realizations content and styles

--- components/main_page/our_realizations.php
+++ components/main_page/our_realizations.php
@@ -1,6 +1,23 @@
 <section id="our-realizations">
     <h2 class="mainpage-title">Nasze realizacje</h2>
-    <div>
-        
+    <div class="images-section">
+        <div class="images-section__slot">
+            <img src="./assets/img/our_realizations/realizations01.jpeg" alt="realizacja" width="480" height="320">
+        </div>
+        <div class="images-section__slot">
+            <img src="./assets/img/our_realizations/realizations02.jpeg" alt="realizacja" width="480" height="320">
+        </div>
+        <div class="images-section__slot">
+            <img src="./assets/img/our_realizations/realizations03.jpeg" alt="realizacja" width="480" height="320">
+        </div>
+        <div class="images-section__slot">
+            <img src="./assets/img/our_realizations/realizations04.jpeg" alt="realizacja" width="480" height="320">
+        </div>
+        <div class="images-section__slot">
+            <img src="./assets/img/our_realizations/realizations05.jpeg" alt="realizacja" width="480" height="320">
+        </div>
+        <div class="images-section__slot">
+            <img src="./assets/img/our_realizations/realizations06.jpeg" alt="realizacja" width="480" height="320">
+        </div>
     </div>
 </section>
