07132ed3792521197c5a21bd619d90deffbfd373
goldipl <mgodlewski92@gmail.com>
2025-01-13 19:30:29 +0100

    update recommended products card slots

--- components/products_card/products_card_recommended.php
+++ components/products_card/products_card_recommended.php
@@ -3,69 +3,15 @@
         <h2 class="desc_title_box--title">Inne produkty z tej kategorii</h2>
     </div>
     <div class="products_blocks_wrapper">
-        <div class="products_blocks_wrapper__slot">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Antystres "kostka" V2704-08</p>
-            </div>
-            <div class="products_block_wrapper__slot--colors">
- 
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od <span>100,90</span> zł</p>
-            </div>
-            <div class="products_block_wrapper__slot--btn">
-                <a href="#">Więcej</a>
-            </div>
-        </div>
 
         <div class="products_blocks_wrapper__slot">
             <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                <a href="">
+                    <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                </a>
             </div>
             <div class="products_block_wrapper__slot--title">
-                <p>Antystres "kostka" V2704-08</p>
-            </div>
-            <div class="products_block_wrapper__slot--colors">
-   
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od <span>100,90</span> zł</p>
-            </div>
-            <div class="products_block_wrapper__slot--btn">
-                <a href="#">Więcej</a>
-            </div>
-        </div>
-
-        <div class="products_blocks_wrapper__slot">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Antystres "kostka" V2704-08</p>
-            </div>
-            <div class="products_block_wrapper__slot--colors">
-                <div class="color_square white"></div>
-                <div class="color_square darkblue"></div>
-                <div class="color_square red"></div>
-                <div class="color_square yellow"></div>
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od <span>100,90</span> zł</p>
-            </div>
-            <div class="products_block_wrapper__slot--btn">
-                <a href="#">Więcej</a>
-            </div>
-        </div>
-
-        <div class="products_blocks_wrapper__slot">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Antystres "kostka" V2704-08</p>
+                <a href="">Antystres "kostka" V2704-08</a>
             </div>
             <div class="products_block_wrapper__slot--colors">
                 <div class="color_square white"></div>
@@ -76,28 +22,95 @@
                 <div class="color_square purple"></div>
             </div>
             <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od <span>100,90</span> zł</p>
-            </div>
-            <div class="products_block_wrapper__slot--btn">
-                <a href="#">Więcej</a>
+                <p class="price"><span>100,90</span> zł</p>
             </div>
         </div>
 
         <div class="products_blocks_wrapper__slot">
             <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                <a href="">
+                    <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                </a>
             </div>
             <div class="products_block_wrapper__slot--title">
-                <p>Antystres "kostka" V2704-08</p>
+                <a href="">Antystres "kostka" V2704-08</a>
             </div>
             <div class="products_block_wrapper__slot--colors">
-   
+                <div class="color_square white"></div>
+                <div class="color_square darkblue"></div>
+                <div class="color_square red"></div>
+                <div class="color_square yellow"></div>
+                <div class="color_square green"></div>
+                <div class="color_square purple"></div>
             </div>
             <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od <span>100,90</span> zł</p>
+                <p class="price"><span>100,90</span> zł</p>
             </div>
-            <div class="products_block_wrapper__slot--btn">
-                <a href="#">Więcej</a>
+        </div>
+
+        <div class="products_blocks_wrapper__slot">
+            <div class="products_block_wrapper__slot--img">
+                <a href="">
+                    <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                </a>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <a href="">Antystres "kostka" V2704-08</a>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square white"></div>
+                <div class="color_square darkblue"></div>
+                <div class="color_square red"></div>
+                <div class="color_square yellow"></div>
+                <div class="color_square green"></div>
+                <div class="color_square purple"></div>
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price"><span>100,90</span> zł</p>
+            </div>
+        </div>
+
+        <div class="products_blocks_wrapper__slot">
+            <div class="products_block_wrapper__slot--img">
+                <a href="">
+                    <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                </a>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <a href="">Antystres "kostka" V2704-08</a>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square white"></div>
+                <div class="color_square darkblue"></div>
+                <div class="color_square red"></div>
+                <div class="color_square yellow"></div>
+                <div class="color_square green"></div>
+                <div class="color_square purple"></div>
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price"><span>100,90</span> zł</p>
+            </div>
+        </div>
+
+        <div class="products_blocks_wrapper__slot">
+            <div class="products_block_wrapper__slot--img">
+                <a href="">
+                    <img src="./assets/img/recommend/yellow_square.jpg" alt="Antystres kostka V2704-08">
+                </a>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <a href="">Antystres "kostka" V2704-08</a>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square white"></div>
+                <div class="color_square darkblue"></div>
+                <div class="color_square red"></div>
+                <div class="color_square yellow"></div>
+                <div class="color_square green"></div>
+                <div class="color_square purple"></div>
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price"><span>100,90</span> zł</p>
             </div>
         </div>
 
