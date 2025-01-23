b8643fe37c431b57f3afdb070e619905e84d5cf1
goldipl <mgodlewski92@gmail.com>
2025-01-23 11:29:17 +0100

    update order styles change one line 5 steps

--- components/main_page/order_process.php
+++ components/main_page/order_process.php
@@ -54,5 +54,19 @@
                 <p>Aliqua incididunt elit ea deserunt magna anim aute ullamco</p>
             </div>
         </div>
+        <div class="order-process_container__arrow">
+            <img src="./assets/img/order_process/white-arrow-on-blue.jpg" alt="strzałka" width="98" height="98">
+        </div>
+        <div class="order-process_container__slot">
+            <div class="order-process_container__slot--img">
+                <img src="./assets/img/order_process/blue-cart.jpg" alt="koszyk" width="64" height="64">
+            </div>
+            <div class="order-process_container__slot--title">
+                <p>Krok 5</p>
+            </div>
+            <div class="order-process_container__slot--text">
+                <p>Aliqua incididunt elit ea deserunt magna anim aute ullamco</p>
+            </div>
+        </div>
     </div>
 </section>
